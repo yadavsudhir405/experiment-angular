217365408a2d9a2250a4c635e6437fccd1ed8fa3
Sudhir Yadav <yadavsudhir405@gmail.com>
2025-03-25 15:35:07 +0530

    With Multiple themes

--- src/app/feature/header/header.component.ts
+++ src/app/feature/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
@@ -32,19 +32,25 @@ export type ThemeDetail = {
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   themes: ThemeDetail[] = [
-    {type: Theme.light, label: 'Light', icon: 'light_mode', class:'light'},
-    {type: Theme.dark, label: 'Dark', icon: 'dark_mode', class: 'dark'},
-    {type: Theme.system, label: 'System', icon: 'desktop_windows', class: 'light dark'}
+    {type: Theme.light, label: 'Orange', icon: 'light_mode', class:'orange-theme'},
+    {type: Theme.dark, label: 'Green', icon: 'dark_mode', class: 'green-theme'},
+    {type: Theme.system, label: 'Deep Brown', icon: 'desktop_windows', class: 'deep-brown-theme'}
   ];
-  selectedTheme: ThemeDetail = this.themes[2];
+  selectedTheme!: ThemeDetail;
 
   constructor() {}
 
+  ngOnInit(): void {
+    this.setTheme(this.themes[0]);
+  }
+
   setTheme(theme: ThemeDetail) {
+    const previousTheme = this.selectedTheme;
     this.selectedTheme = theme;
-    document.body.style.colorScheme = theme.class;
+    document.body.classList.remove(previousTheme?.class);
+    document.body.classList.add(theme.class);
   }
 
   onMenuItemClick(theme: ThemeDetail) {
