f0d1862e168178b938d24dcd0c82ef34694de439
Sudhir Yadav <yadavsudhir405@gmail.com>
2025-03-31 17:01:12 +0530

    Add custom Inter font family instead of default Roboto

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
         "sys-on-background": "var(--mat-sys-on-background)",
         "sys-primary": "var(--mat-sys-primary)",
         "sys-on-primary": "var(--mat-sys-on-primary)",
+      },
+      texts: {
+        "very-large": '2rem',
       }
     },
   },
